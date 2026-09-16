08802d5bdc4558a9df342b6bf67c5d5a5f254583
dedestem <david@davidnet.net>
2026-09-16 21:54:28 +0200

    Add backfill

---
+++ src/routes/backfill/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { postFetch } from "@davidnet-net/svelte-ui";
+	import { onMount } from "svelte";
+
+	onMount(() => {
+		postFetch(
+			"https://davidnet-backend.davidnet.net/social/shorts/admin/backfill-durations",
+			undefined,
+			undefined,
+			true
+		);
+	});
+</script>
